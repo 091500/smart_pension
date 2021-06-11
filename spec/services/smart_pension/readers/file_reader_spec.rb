92dc84f88f71df9a6af1780d682494f3c8cf87d4
Ivan Romanenko <iromanenko@beltechsoftware.com>
2021-06-12 01:19:20 +0300

    Updated FileReader spec.

--- spec/services/smart_pension/readers/file_reader_spec.rb
+++ spec/services/smart_pension/readers/file_reader_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 RSpec.describe SmartPension::Readers::FileReader do
-  let(:file_path) { Rails.root.join(*%w[spec fixtures files webserver.example.log]) }
+  let(:file_path) { Rails.root.join(*%w[spec fixtures files webserver.log]) }
+  let(:bad_name_file_path) { Rails.root.join(*%w[spec fixtures files webserver.example.log]) }
 
   context 'file exists' do
     it 'generates an array of valid LogEntry objects' do
@@ -16,6 +17,13 @@ RSpec.describe SmartPension::Readers::FileReader do
   context 'file absent' do
     it 'generates error message' do
       expect { described_class.new(file_path: 'nil').each { |log_entry| log_entry } }.to raise_error(Errno::ENOENT)
+    end
+  end
+
+  context 'file name is not webserver.log' do
+    it 'generates error message' do
+      expectation = expect { described_class.new(file_path: bad_name_file_path).each { |log_entry| log_entry } }
+      expectation.to raise_error(StandardError)
     end
   end
 end
