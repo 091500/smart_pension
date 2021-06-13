583be75768fc14a884625c304220c266d272cc59
Ivan Romanenko <iromanenko@beltechsoftware.com>
2021-06-13 20:02:40 +0300

    Added ResultEntry test.

---
+++ test/models/smart_pension/result_entry_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+require 'ostruct'
+
+module SmartPension
+  class ResultEntryTest < ActiveSupport::TestCase
+    test '#initialize' do
+      assert ResultEntry.new
+    end
+
+    test '<< when passed invalid' do
+      instance = ResultEntry.new
+      instance << ''
+      instance << OpenStruct.new
+      instance << OpenStruct.new(page_path: '/page')
+      instance << OpenStruct.new(ip_address: '1.2.3.4')
+
+      assert_equal({}, instance.uniq_pages)
+      assert_equal({}, instance.uniq_views)
+    end
+
+    test '#<< when passed valid' do
+      instance = ResultEntry.new
+      instance << OpenStruct.new(page_path: '/page', ip_address: '1.2.3.4')
+      instance << OpenStruct.new(page_path: '/page', ip_address: '1.2.3.5')
+      instance << OpenStruct.new(page_path: '/page', ip_address: '1.2.3.5')
+      instance << OpenStruct.new(page_path: '/test', ip_address: '1.2.3.4')
+
+      assert_equal({ '/page' => %w[1.2.3.4 1.2.3.5 1.2.3.5], '/test' => %w[1.2.3.4] }, instance.uniq_pages)
+      assert_equal({ '/page' => %w[1.2.3.4 1.2.3.5], '/test' => %w[1.2.3.4] }, instance.uniq_views)
+    end
+  end
+end
